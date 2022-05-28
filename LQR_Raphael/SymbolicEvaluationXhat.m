163412025e2b29ca2f87d45f90d5123f45d4434b
Raphael Boitte <raphael.boitte@gmail.com>
2022-05-28 10:34:33 +0200

    Linear kalman filter working but position xyz unobservable

---
+++ LQR_Raphael/SymbolicEvaluationXhat.m
@@ -0,0 +1,14 @@
+function [xhat_kplus1,A]=SymbolicEvaluationXhat(xhat_k,u_k)
+%% Obtaining symbolic function
+[F,X,U]=OtterFunction_EKF(); %xdot=f(x,u);
+A=jacobian(F,X); %Compute A matrix of ekf, symbolic form
+
+
+%% Eval xhat
+xhat_kplus1= OtterFunction_lqr(xhat_k,u_k,0,0,0,0);
+
+A=subs(A, X, xhat_kplus1);
+A=subs(A,U,u_k);
+A=double(A);% Cast it to double
+
+end
