c19efc60ffb90c53ea37b862cbd4fc64b701aacd
Raphael Boitte <raphael.boitte@gmail.com>
2022-06-19 10:37:12 +0200

    changed KF

--- LQR_Raphael/SymbolicEvaluationXhat.m
+++ LQR_Raphael/SymbolicEvaluationXhat.m
@@ -2,11 +2,6 @@ function [A]=SymbolicEvaluationXhat(xhat_kplus1,Thrust)
 %% Obtaining symbolic function
 [F,X,U]=OtterFunction_EKF(); %xdot=f(x,u);
 A=jacobian(F,X); %Compute A matrix of ekf, symbolic form
-
-
-%% Eval xhat
-% xhat_kplus1= OtterFunction_lqr(xhat_k,u_k,0,0,0,0);
-
 A=subs(A, X, xhat_kplus1);
 A=subs(A,U,Thrust);
 A=double(A);% Cast it to double
